bcf543e9859deff24ecb27ee48ac61a29907e9a1
abu1136 <36995117+abu1136@users.noreply.github.com>
2026-01-25 00:46:07 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.routers import auth, documents, admin
 
-app = FastAPI()
+app = FastAPI(title="Document Manager")
 
 app.include_router(auth.router)
 app.include_router(documents.router)
